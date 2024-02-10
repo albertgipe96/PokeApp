acf92e83a814389a23958e173fe6cff2824d5139
albertgipe96 <albertgipe96@gmail.com>
2024-02-10 19:49:32 +0100

    Set basic colors

--- app/src/main/java/com/example/pokeapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/pokeapp/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// App Colors
+val black = Color(0xFF0B090A)
+val neutral_900 = Color(0xFF161A1D)
+val neutral_300 = Color(0xFFB1A7A6)
+val neutral_200 = Color(0xFFD3D3D3)
+val neutral_100 = Color(0xFFF5F3F4)
+val white = Color(0xFFFFFFFF)
+
+// Primary colors
+val red_900 = Color(0xFF660708)
+val red_300 = Color(0xFFA4161A)
+val red_200 = Color(0xFFBA181B)
+val red_100 = Color(0xFFE5383B)
