6e6eb02975bb01ad4f82cdba38e31d50b68797be
albertgipe96 <albertgipe96@gmail.com>
2024-02-18 12:57:29 +0100

    Shape and Spacing in theme

---
+++ app/src/main/java/com/example/pokeapp/ui/theme/Spacing.kt
@@ -0,0 +1,13 @@
+package com.example.pokeapp.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class Spacing(val spacing: Dp) {
+    EXTRA_SMALL(spacing = 4.dp),
+    SMALL(8.dp),
+    SMALL_MEDIUM(spacing = 12.dp),
+    MEDIUM(spacing = 16.dp),
+    MEDIUM_LARGE(spacing = 24.dp),
+    LARGE(spacing = 32.dp)
+}
