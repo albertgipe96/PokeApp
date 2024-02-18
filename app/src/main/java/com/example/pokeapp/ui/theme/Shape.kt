a4dc8dedc5253e49c068d9a52f163541c2bd7907
albertgipe96 <albertgipe96@gmail.com>
2024-02-18 13:08:31 +0100

    Shape fix

--- app/src/main/java/com/example/pokeapp/ui/theme/Shape.kt
+++ app/src/main/java/com/example/pokeapp/ui/theme/Shape.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(6.dp),
-    large = RoundedCornerShape(8.dp),
-)
+enum class Shape(val shape: RoundedCornerShape) {
+    small(RoundedCornerShape(4.dp)),
+    medium(RoundedCornerShape(6.dp)),
+    large(RoundedCornerShape(8.dp)),
+}
