338bf7ccffeddd0afc2e20a1af687fffc69a5d0a
albertgipe96 <albertgipe96@gmail.com>
2024-02-18 01:37:20 +0100

    Type icons

---
+++ app/src/main/java/com/example/pokeapp/ui/theme/Icons.kt
@@ -0,0 +1,25 @@
+package com.example.pokeapp.ui.theme
+
+import androidx.annotation.DrawableRes
+import com.example.pokeapp.R
+
+enum class Icons(@DrawableRes val drawable: Int) {
+    TYPE_BUG(R.drawable.ic_bug),
+    TYPE_DARK(R.drawable.ic_dark),
+    TYPE_DRAGON(R.drawable.ic_dragon),
+    TYPE_ELECTRIC(R.drawable.ic_electric),
+    TYPE_FAIRY(R.drawable.ic_fairy),
+    TYPE_FIGHTING(R.drawable.ic_fighting),
+    TYPE_FIRE(R.drawable.ic_fire),
+    TYPE_FLYING(R.drawable.ic_flying),
+    TYPE_GHOST(R.drawable.ic_ghost),
+    TYPE_GRASS(R.drawable.ic_grass),
+    TYPE_GROUND(R.drawable.ic_ground),
+    TYPE_ICE(R.drawable.ic_ice),
+    TYPE_NORMAL(R.drawable.ic_normal),
+    TYPE_POISON(R.drawable.ic_poison),
+    TYPE_PSYCHIC(R.drawable.ic_psychic),
+    TYPE_ROCK(R.drawable.ic_rock),
+    TYPE_STEEL(R.drawable.ic_steel),
+    TYPE_WATER(R.drawable.ic_water),
+}
